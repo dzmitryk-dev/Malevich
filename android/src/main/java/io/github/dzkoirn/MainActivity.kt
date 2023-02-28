03a1c419aea30c207ef5a298cc7319df29932be6
Dzmitry Kalianchuk <koirn.dev@gmail.com>
2023-02-28 23:47:41 +0100

    Runnable Android App

--- android/src/main/java/io/github/dzkoirn/MainActivity.kt
+++ android/src/main/java/io/github/dzkoirn/MainActivity.kt
@@ -1,16 +1,23 @@
 package io.github.dzkoirn
 
+import android.content.Context
 import android.os.Bundle
+import android.util.DisplayMetrics
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.Text
+import malunki.First
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        val (width, height) = with(displayMetrics) { widthPixels to heightPixels }
+
         setContent {
-            Text("Hello world!")
+            First(width, height)
         }
     }
 }
