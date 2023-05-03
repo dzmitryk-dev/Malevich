9dd4506f315a9abd0362524b1d477641dc03de1a
Dzmitry Kalianchuk <koirn.dev@gmail.com>
2023-05-03 16:28:13 +0200

    ChatGPT experiment - implemented zoom in

--- common/src/commonMain/kotlin/malunki/MandelbrotChatGpt.kt
+++ common/src/commonMain/kotlin/malunki/MandelbrotChatGpt.kt
@@ -9,11 +9,14 @@ import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.*
 import androidx.compose.ui.graphics.drawscope.Stroke
 import kotlinx.coroutines.delay
+import kotlin.math.sqrt
+import kotlin.random.Random
 
 @Composable
 fun MandelbrotChatGpt(width: Int, height: Int) {
     var imageBitmap by remember { mutableStateOf(ImageBitmap(width, height)) }
     var frame by remember { mutableStateOf(Pair(Offset.Zero, Offset(x = width.toFloat(), y = height.toFloat()))) }
+    var center by remember { mutableStateOf(Offset(x = width.toFloat()/2, y = height.toFloat()/2)) }
 
     Canvas(modifier = Modifier.fillMaxSize()) {
         drawImage(imageBitmap)
@@ -23,6 +26,7 @@ fun MandelbrotChatGpt(width: Int, height: Int) {
             size = Size(width = frame.second.x - frame.first.x, height = frame.second.y - frame.first.y),
             style = Stroke(1.0f)
         )
+        drawCircle(Color.Red, radius = 1.0f, center = center)
     }
 
     LaunchedEffect(Unit) {
@@ -34,18 +38,29 @@ fun MandelbrotChatGpt(width: Int, height: Int) {
         imageBitmap = fromPixelMap(width, height,
             generateMandelbrotImage(width, height, xMin, xMax, yMin, yMax)
         )
-        delay(1000L)
+        delay(100L)
 
-        val numImages = 100
+        val (xCenter, yCenter) = findBoundaryOnLine(
+            maxIterations = 1000,
+            xmin = xMin,
+            xmax = xMax,
+            ymin = yMin,
+            ymax = yMax
+        )
+        val sizeFactor = 0.9 //0.1 + 0.8 * Math.random()
+
+        val numIterations = 100
 
         // генерируем указанное количество изображений
-        repeat(numImages) {
-            // выбираем случайную область на границе множества Мандельброта
+        repeat(numIterations) {
+            center = Offset(
+                x = ((xCenter - xMin) * width / (xMax - xMin)).toFloat(),
+                y = ((yCenter - yMin) * height / (yMax - yMin)).toFloat()
+            )
+            delay(100L)
+
             val xRange = xMax - xMin
             val yRange = yMax - yMin
-            val xCenter = xMin + xRange * Math.random()
-            val yCenter = yMin + yRange * Math.random()
-            val sizeFactor = 0.1 + 0.4 * Math.random()
             val xSize = xRange * sizeFactor
             val ySize = yRange * sizeFactor
 
@@ -67,7 +82,7 @@ fun MandelbrotChatGpt(width: Int, height: Int) {
                     y = ((yMax - newyMin) * yScale).toFloat()
                 )
             )
-            delay(1000L)
+            delay(50L)
 
             xMin = newxMin
             xMax = newxMax
@@ -77,7 +92,7 @@ fun MandelbrotChatGpt(width: Int, height: Int) {
             imageBitmap = fromPixelMap(width, height,
                 generateMandelbrotImage(width, height, xMin, xMax, yMin, yMax))
             frame = Pair(Offset.Zero, Offset(x = width.toFloat(), y = height.toFloat()))
-            delay(1000L)
+            delay(50L)
         }
     }
 }
@@ -133,4 +148,77 @@ private fun fromPixelMap(width: Int, height: Int, colorMapping: Array<List<Offse
         }
     }
 }
+
+private fun findBoundaryOnLine(
+    maxIterations: Int = 1000,
+    threshold: Double = 0.000001,
+    xmin: Double = -2.0,
+    xmax: Double = 1.0,
+    ymin: Double = -1.5,
+    ymax: Double = 1.5
+): Pair<Double, Double> {
+    fun isMandelbrotPointOnBoundary(
+        x: Double,
+        y: Double,
+        maxIterations: Int
+    ): Boolean {
+        var real = 0.0
+        var imag = 0.0
+        for (i in 0 until maxIterations) {
+            val tempReal = real * real - imag * imag + x
+            val tempImag = 2 * real * imag + y
+            real = tempReal
+            imag = tempImag
+            if (real * real + imag * imag >= 4) {
+                return false // точка не находится на границе множества Мандельброта
+            }
+        }
+        return true // точка находится на границе множества
+    }
+
+    // Генерируем две точки на плоскости
+    var (x1, y1) = if (Random.nextBoolean()) {
+        if (Random.nextBoolean()) {
+            xmin
+        } else {
+            xmax
+        } to Random.nextDouble(ymin, ymax)
+    } else {
+        Random.nextDouble(xmin, xmax) to if (Random.nextBoolean()) {
+            ymin
+        } else {
+            ymax
+        }
+    }
+
+    var x2: Double = 0.0
+    var y2: Double = 0.0
+
+    var iteration = 0
+    do {
+        // Строим отрезок между двумя точками
+        val dx = x2 - x1
+        val dy = y2 - y1
+
+        // Вычисляем координаты точки на отрезке с заданным параметром t
+        val x = x1 + dx / 2
+        val y = y1 + dy / 2
+
+        val isOnBoundary = isMandelbrotPointOnBoundary(x, y, maxIterations)
+        // Изменяем границы поиска
+        if (isOnBoundary) {
+            x2 = x
+            y2 = y
+        } else {
+            x1 = x
+            y1 = y
+        }
+        iteration++
+    } while (sqrt(dx*dx + dy*dy) > threshold || iteration < maxIterations)
+    return Pair(x1 + (x2 - x1) / 2, y1 + (y2 - y1) /2)
+}
+
+
+
+
 
