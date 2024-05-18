1313e67a883efe9709197611782406ae1aadca48
Dzmitry Kalianchuk <koirn.dev@gmail.com>
2024-05-18 11:30:14 +0200

    WIP

--- compose-app/src/desktopMain/kotlin/Main.kt
+++ compose-app/src/desktopMain/kotlin/Main.kt
@@ -1,16 +1,10 @@
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalViewConfiguration
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-import androidx.compose.ui.window.rememberWindowState
 import malunki.*
 import mu.KotlinLogging
-import kotlin.math.roundToInt
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication, title = "Malevich for Desktop") {
---
+++ compose-app/src/wasmJsMain/kotlin/main.kt
@@ -0,0 +1,17 @@
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.window.CanvasBasedWindow
+import malunki.RotatingLine2
+
+@OptIn(ExperimentalComposeUiApi::class)
+fun main() {
+    CanvasBasedWindow(canvasElementId = "ComposeTarget") {
+        Box(modifier = Modifier.fillMaxSize()) {
+//            First()
+//            RotatingLine()
+            RotatingLine2()
+        }
+    }
+}
